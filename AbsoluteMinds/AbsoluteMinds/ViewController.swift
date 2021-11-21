842a8ccbc635760676aa2284af8ec49471922940
Abdullah Bajaman <ambajaman@gmail.com>
2021-11-21 16:22:52 +0300

    initial project

---
+++ AbsoluteMinds/AbsoluteMinds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AbsoluteMinds
+//
+//  Created by Abdullah Bajaman on 21/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
