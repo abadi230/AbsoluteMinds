fd2d34c543d22d23ff51d0373f09597aefdb20ec
MohammedMabruk <Mohammed.Mabruk.med.s@tuwaiq.edu.sa>
2021-11-23 14:53:09 +0300

    Add ScrollView Add Labels and Button Outlets

---
+++ AbsoluteMinds/AbsoluteMinds/DetailsVC.swift
@@ -0,0 +1,33 @@
+//
+//  DetailsVC.swift
+//  AbsoluteMinds
+//
+//  Created by Mohammed Mabruk on 18/04/1443 AH.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+    @IBOutlet weak var titlelLable: UILabel!
+    @IBOutlet weak var authorLable: UILabel!
+    
+    @IBOutlet weak var dateLable: UILabel!
+    @IBOutlet weak var imageVeiw: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
